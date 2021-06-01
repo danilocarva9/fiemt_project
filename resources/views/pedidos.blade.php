cb01e954035e0b710aa84c368bfbf55be3bf08ef
Danilo Carvalho <danilocarva9@gmail.com>
2021-05-31 23:42:57 -0400

    better go to sleep

---
+++ resources/views/pedidos.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="table-responsive">
+<table class="table table-striped table-sm">
+<thead>
+<tr>
+<th>#</th>
+<th>Cliente</th>
+<th>Produto</th>
+<th>Qtd.</th>
+<th>Valor Unitário</th>
+<th>Valor Total</th>
+<th>Opções</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>1,001</td>
+<td>random</td>
+<td>data</td>
+<td>placeholder</td>
+<td>text</td>
+<td>text</td>
+<td>options</td>
+</tr>
+</tbody>
+</table>
+</div>
+
+@endsection
